c22e037c2a8f4c31eb7492b092c9e2957f11bfca
Anshul Jain <mosquito242@gmail.com>
2015-02-24 23:19:53 -0800

    initial commit

---
+++ netbin.py
@@ -0,0 +1,15 @@
+import click
+
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+
+def netbin(count, name):
+	for x in range(count):
+		click.echo('Hello %s!' % name)
+
+
+if __name__ == '__main__':
+    netbin()
---
+++ readme.md
@@ -0,0 +1,5 @@
+Anshul Jain (AJ) and Marc Lindsay
+
+One thing I realized we need to do - 
+
+	-we'll definitely need a host process running netbin host as a server socket managing connections and the file list, otherwise it's not possible to have a background process autostart.
