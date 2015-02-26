aaaa038c7395113a5c44558ba7609f40121ebc85
Anshul Jain <mosquito242@gmail.com>
2015-02-25 21:24:44 -0800

    added multithreading, ping

--- netbin.py
+++ netbin.py
@@ -1,15 +1,29 @@
-import click
+import socket
+from multiprocessing.pool import Pool
+import pingy
+
+def ping(host):
+    # ping = subprocess.Popen(['ping', '-w', '500', host],
+    #                         stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    # out, error = ping.communicate()
+    # return (out, error)
+    pingy.verbose_ping(host, 1, 1)
+    return host
 
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
+addresses = ['192.168.0.1', '192.168.0.2', '192.168.0.8'] # etc.
+pool = Pool(10) # Increase number to increase speed and resource consumption
+ping_results = pool.map(ping, addresses)
+print(ping_results)
 
-def netbin(count, name):
-	for x in range(count):
-		click.echo('Hello %s!' % name)
+pool.close()
+pool.join()
 
 
-if __name__ == '__main__':
-    netbin()
+# def netbin(count, name):
+
+	
+
+
+# if __name__ == '__main__':
+#     netbin()
