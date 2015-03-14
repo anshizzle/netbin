8003a40722672cd90b65c039784da929945a72ef
lindsayemarc@gmail.com <lindsayemarc@gmail.com>
2015-03-14 09:45:22 -0700

    wrote to disk

--- netbin_tcp.py
+++ netbin_tcp.py
@@ -7,12 +7,13 @@ import constants
 from util import *
 
 
+
 class netbin_tcp:
 
 	def __init__(self, port):
 		self.port = port
 
-	def tcp_listener(self, comm_addr, udp_sock):
+	def tcp_listener(self, comm_addr, fh,  udp_sock):
 		print "YOU ARE NOW LISTENING FOR FILE DATA ON PORT: "+ str(self.port)
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.bind((socket.gethostname(), self.port))
@@ -21,17 +22,28 @@ class netbin_tcp:
 		# tell the requesting client the connection is open and what port it's on
 		udp_sock.send_tcp_open_msg(self.port, comm_addr)		
 		con, file_owner_addr = sock.accept()
+
+		# actually read the data
 		file_data = ""
+
+		try:
+
+			f = open(fh, 'w')
+
+		except IOError:
+
+			printDebug("check that file exists", "ioe")
+
 		while not file_data.endswith(constants.FILE_END_SIGNAL):
 				try:
 					file_data = con.recv(constants.GEN_PACKET_LENGTH)
-					print file_data
+					if file_data.endswith(constants.FILE_END_SIGNAL):
+						file_data = file_data[0:len(file_data)-len(constants.FILE_END_SIGNAL)]
+					f.write(file_data)
 				except socket.error as serr:
 					printDebug("Socket Error " + str(serr.errno))
-				
 					
-		
-
+		f.close()
 		con.close()
 
 
@@ -46,7 +58,7 @@ class netbin_tcp:
 				print fd
 
 		except IOError:
-			print "check that the file exists"
+			printDebug("check that the file exists","ioe")
 
 
 		try:
